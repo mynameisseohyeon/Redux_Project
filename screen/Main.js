8336c7e7642276a37e7b86a753fd170a896c6ff7
seohyeon <uimi3606@gmail.com>
2023-07-16 16:49:54 +0900

    DESIGN 매인 페이지 구조 완성

--- screen/Main.js
+++ screen/Main.js
@@ -1,4 +1,5 @@
 import {View, Text, StyleSheet} from 'react-native';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function Main() {
   return (
@@ -6,11 +7,23 @@ function Main() {
       <View>
         <Text style={[styles.Header]}>Home</Text>
         <View style={[styles.Collection]}>
-          <Text style={[styles.colMainText]}>New Collection</Text>
-          <Text style={[styles.colSubText]}>Discount 50% </Text>
-          <Text style={[styles.colSubText]}>for the first transaction</Text>
-          <Text style={[styles.shopNow]}>shop now</Text>
-          <i class="fi fi-rr-picture"></i>
+          <View>
+            <Text style={[styles.colMainText]}>New Collection</Text>
+            <Text style={[styles.colSubText]}>Discount 50% </Text>
+            <Text style={[styles.colSubText]}>for the first transaction</Text>
+            <Text style={[styles.shopNow]}>shop now</Text>
+          </View>
+          {/* <View>
+            <Icon name="collections" size={32} color="white" />
+          </View> */}
+        </View>
+
+        <View style={[styles.Brand]}>
+          <Text style={[styles.BrandMainText]}>Brand</Text>
+        </View>
+
+        <View style={[styles.NewArrival]}>
+          <Text style={[styles.NewArrivalText]}>New Arrival</Text>
         </View>
       </View>
     </>
@@ -35,6 +48,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     borderRadius: 10,
     padding: 20,
+    marginBottom: 40,
   },
   colMainText: {
     color: 'white',
@@ -51,6 +65,29 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     fontSize: 25,
+  },
+  Brand: {
+    width: '100%',
+    height: 180,
+    backgroundColor: '#999',
+    position: 'relative',
+    padding: 20,
+    marginBottom: 10,
+  },
+  BrandMainText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  NewArrival: {
+    width: '100%',
+    height: 180,
+    backgroundColor: '#999',
+    position: 'relative',
+    padding: 20,
+  },
+  NewArrivalText: {
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
 
