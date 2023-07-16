d599e14aa692f69819f365a9a0469e67ad04624e
seohyeon <uimi3606@gmail.com>
2023-07-16 16:29:59 +0900

    DESIGN 벡터 아이콘 라이브러리 추가

--- screen/Main.js
+++ screen/Main.js
@@ -1,11 +1,57 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function Main() {
   return (
-    <View>
-      <Text>안녕하세요</Text>
-    </View>
+    <>
+      <View>
+        <Text style={[styles.Header]}>Home</Text>
+        <View style={[styles.Collection]}>
+          <Text style={[styles.colMainText]}>New Collection</Text>
+          <Text style={[styles.colSubText]}>Discount 50% </Text>
+          <Text style={[styles.colSubText]}>for the first transaction</Text>
+          <Text style={[styles.shopNow]}>shop now</Text>
+          <i class="fi fi-rr-picture"></i>
+        </View>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  Header: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#999',
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 5,
+  },
+  Collection: {
+    width: 400,
+    height: 200,
+    backgroundColor: '#777',
+    right: -5,
+    top: 20,
+    position: 'relative',
+    borderRadius: 10,
+    padding: 20,
+  },
+  colMainText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 28,
+    paddingBottom: 10,
+  },
+  colSubText: {
+    color: '#999',
+    fontSize: 20,
+    paddingBottom: 10,
+  },
+  shopNow: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+});
 
 export default Main;
